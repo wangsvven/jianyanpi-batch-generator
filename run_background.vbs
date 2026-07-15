4f2859d898f484510d039e4ef0fd714359d50464
wangsvven <wangsvven@users.noreply.github.com>
2026-07-15 22:58:22 +0800

    恢复 run_background.vbs 和 start_platform.bat

---
+++ run_background.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "start_platform.bat", 0, False
---
+++ start_platform.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\Administrator\WorkBuddy\2026-07-15-11-28-33
+call venv\Scripts\activate.bat
+cd src
+python app.py
